2e18110d4a5b1d1c846851694887ffd206c51711
SM-junior <shahinalam12341@gmail.com>
2023-10-17 00:46:11 +0600

    new user is deleted from ui and database

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,16 +1,37 @@
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
-    const users=useLoaderData();
+    const users = useLoaderData();
+
+    const handleDelete = _id => {
+        console.log(_id);
+        fetch(`http://localhost:3000/users/${_id}`,{
+            method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.deletedCount>0){
+                alert('User deleted successfully')
+            }
+            console.log(data);
+        })
+    }
 
     return (
         <div>
             <h2>Total users: {users.length}</h2>
-            {
-                users.map(user=>
-                    <p style={{textAlign:'left'}} key={user._id}>Name: {user.name}, Email:{user.email}, ID: {user._id}</p>
-                )
-            }
+            <div>
+                {
+                    users.map(user =>
+                        <div key={user._id}>
+                            <p
+                                style={{ textAlign: 'left' }}
+                            >Name: {user.name}, Email:{user.email}, ID: {user._id} <button onClick={() => handleDelete(user._id)}>x</button></p>
+                        </div>
+
+                    )
+                }
+            </div>
         </div>
     );
 };
